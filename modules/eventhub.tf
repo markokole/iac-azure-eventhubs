8422dd04b7c852f1151cf063457d48fb489cdc88
Marko Kljajic <marko_kole@yahoo.com>
2019-07-21 20:19:53 +0200

    created locals block

--- modules/eventhub.tf
+++ modules/eventhub.tf
@@ -1,13 +1,20 @@
 provider "azurerm" {
 }
 
+locals {
+    namespace = "markobigdata"
+    resource_group_name = "eventHubRG"
+    location = "westeurope"
+    topic = "testtopic"
+}
+
 resource "azurerm_resource_group" "event_hub_test" {
-    name                    = "eventHubRG"
-    location                = "westeurope"
+    name                    = "${local.resource_group_name}"
+    location                = "${local.location}"
 }
 
 resource "azurerm_eventhub_namespace" "event_hub_test" {
-    name                    = "markoTerraformEventHubNS123"
+    name                    = "${local.namespace}" #"markoTerraformEventHubNS123"
     location                = "${azurerm_resource_group.event_hub_test.location}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
     #sku                     = "Basic" #if kafka not enabled
@@ -17,7 +24,7 @@ resource "azurerm_eventhub_namespace" "event_hub_test" {
 }
 
 resource "azurerm_eventhub" "event_hub_test" {
-    name                    = "markoTerraformEventHub123"
+    name                    = "${local.topic}" #"markoTerraformEventHub123"
     namespace_name          = "${azurerm_eventhub_namespace.event_hub_test.name}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
     partition_count         = 2
@@ -25,7 +32,7 @@ resource "azurerm_eventhub" "event_hub_test" {
 }
 
 resource "azurerm_eventhub_namespace_authorization_rule" "event_hub_test" {
-    name                    = "markoTerraformAuthRule123"
+    name                    = "${local.namespace}-auth" #"markoTerraformAuthRule123"
     namespace_name          = "${azurerm_eventhub_namespace.event_hub_test.name}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
 
