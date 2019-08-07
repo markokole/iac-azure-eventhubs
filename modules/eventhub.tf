490ffc861dba5f64b0e055c713e7aa8b3451d088
Marko Kljajic <marko_kole@yahoo.com>
2019-08-07 11:59:36 +0200

    added capture_description logic

--- modules/eventhub.tf
+++ modules/eventhub.tf
@@ -1,20 +1,13 @@
 provider "azurerm" {
 }
 
-locals {
-    namespace = "markobigdata"
-    resource_group_name = "eventHubRG"
-    location = "westeurope"
-    topic = "testtopic"
-}
-
 resource "azurerm_resource_group" "event_hub_test" {
     name                    = "${local.resource_group_name}"
     location                = "${local.location}"
 }
 
 resource "azurerm_eventhub_namespace" "event_hub_test" {
-    name                    = "${local.namespace}" #"markoTerraformEventHubNS123"
+    name                    = "${local.eventhub_namespace}"
     location                = "${azurerm_resource_group.event_hub_test.location}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
     #sku                     = "Basic" #if kafka not enabled
@@ -24,18 +17,32 @@ resource "azurerm_eventhub_namespace" "event_hub_test" {
 }
 
 resource "azurerm_eventhub" "event_hub_test" {
-    name                    = "${local.topic}" #"markoTerraformEventHub123"
+    depends_on = ["azurerm_storage_container.test"]
+    count = 5
+    name                    = "prod.test${count.index + 1}"
     namespace_name          = "${azurerm_eventhub_namespace.event_hub_test.name}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
-    partition_count         = 2
-    message_retention       = 1
+    partition_count         = "${local.partition_count}"
+    message_retention       = "${local.message_retention}"
+    capture_description = {
+        enabled             = true
+        encoding            = "${local.capture_desc_encoding}"
+        interval_in_seconds = 60
+        size_limit_in_bytes = 10485760
+        skip_empty_archives = true
+        destination         = {
+            name                    = "${local.capture_desc_name}"
+            blob_container_name     = "${azurerm_storage_container.test.name}"
+            storage_account_id      = "${azurerm_storage_account.test.id}"
+            archive_name_format     = "${local.archive_name_format}"
+        }
+    }
 }
 
 resource "azurerm_eventhub_namespace_authorization_rule" "event_hub_test" {
-    name                    = "${local.namespace}-auth" #"markoTerraformAuthRule123"
+    name                    = "${local.eventhub_namespace}-auth"
     namespace_name          = "${azurerm_eventhub_namespace.event_hub_test.name}"
     resource_group_name     = "${azurerm_resource_group.event_hub_test.name}"
-
     manage                  = true
     listen                  = true
     send                    = true
