c7a436e4549598672b65ab83566f9a93038e3531
Marko Kljajic <marko_kole@yahoo.com>
2019-08-11 07:26:36 +0200

    Update blobstorage.tf

--- modules/blobstorage.tf
+++ modules/blobstorage.tf
@@ -1,26 +1,16 @@
 resource "azurerm_storage_account" "test" {
+  count                    = "${local.provision_blob_storage}"
   name                     = "${local.storage_account_name}"
-  resource_group_name      = "${azurerm_resource_group.event_hub_test.name}"
+  resource_group_name      = "${azurerm_resource_group.resource_group.name}"
   location                 = "${local.location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "test" {
-  name                  = "${local.storage_container_name}"
-  resource_group_name   = "${azurerm_resource_group.event_hub_test.name}"
-  storage_account_name  = "${azurerm_storage_account.test.name}"
-  container_access_type = "blob"
+  count                    = "${local.provision_blob_storage}"
+  name                     = "${local.storage_container_name}"
+  resource_group_name      = "${azurerm_resource_group.resource_group.name}"
+  storage_account_name     = "${azurerm_storage_account.test.name}"
+  container_access_type    = "blob"
 }
-
-/*
-resource "azurerm_storage_blob" "blob_storage_kafka" {
-  name = "${local.storage_blob_name}"
-
-  resource_group_name    = "${azurerm_resource_group.event_hub_test.name}"
-  storage_account_name   = "${azurerm_storage_account.test.name}"
-  storage_container_name = "${azurerm_storage_container.test.name}"
-  type                   = "page"
-  size                   = 1024
-}
-*/
