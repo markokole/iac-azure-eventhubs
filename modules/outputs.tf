c54acd90f05d7598e5ec58e1600e379ede5193ff
Marko Kljajic <marko_kole@yahoo.com>
2019-08-11 07:27:13 +0200

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,29 +1,26 @@
 output "resource_group_name" {
-    value = "${azurerm_resource_group.event_hub_test.name}"
+    value           = "${azurerm_resource_group.resource_group.name}"
 }
-
+output "databricks_workspace_id" {
+    depends_on      = ["azurerm_databricks_workspace.databricks_workspace"]
+    value           = "${azurerm_databricks_workspace.databricks_workspace.*.id}"
+}
 output "primary_key" {
-    value = "${azurerm_eventhub_namespace_authorization_rule.event_hub_test.primary_key}"
+    depends_on      = ["azurerm_eventhub_namespace_authorization_rule.event_hub_test"]
+    value           = "${azurerm_eventhub_namespace_authorization_rule.event_hub_test.*.primary_key}"
 }
 
 output "event_hub_namespace" {
-    value = "${azurerm_eventhub_namespace.event_hub_test.name}"
+    depends_on      = ["azurerm_eventhub_namespace.event_hub_test"]
+    value           = "${azurerm_eventhub_namespace.event_hub_test.*.name}"
 }
 
 output "event_hub" {
-    value = ["${azurerm_eventhub.event_hub_test.*.name}"]
+    depends_on      = ["azurerm_eventhub.event_hub_test"]
+    value           = ["${azurerm_eventhub.event_hub_test.*.name}"]
 }
 
 output "primary_connection_string" {
-    value = "${azurerm_eventhub_namespace_authorization_rule.event_hub_test.primary_connection_string}"
+    depends_on      = ["azurerm_eventhub_namespace_authorization_rule.event_hub_test"]
+    value           = "${azurerm_eventhub_namespace_authorization_rule.event_hub_test.*.primary_connection_string}"
 }
-
-/*
-output "storage_blob_id:" {
-    value = "${azurerm_storage_blob.blob_storage_kafka.id}"
-}
-
-output "storage_blob_url:" {
-    value = "${azurerm_storage_blob.blob_storage_kafka.url}"
-}
-*/
