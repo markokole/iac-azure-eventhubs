05269a634378a84e63fce58cedc5c77c540c82a1
Marko Kljajic <marko_kole@yahoo.com>
2019-08-11 07:26:45 +0200

    Create databricks.tf

---
+++ modules/databricks.tf
@@ -0,0 +1,8 @@
+resource "azurerm_databricks_workspace" "databricks_workspace" {
+    count                       = "${local.provision_databricks}"
+    name                        = "${local.databricks_workspace_name}"
+    resource_group_name         = "${azurerm_resource_group.resource_group.name}"
+    managed_resource_group_name = "${local.databricks_managed_resource_group}"
+    location                    = "${azurerm_resource_group.resource_group.location}"
+    sku                         = "standard"
+}
