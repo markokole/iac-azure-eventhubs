ce5785dcf139825c64a883fb693b575127c161a3
Marko Kljajic <marko_kole@yahoo.com>
2019-08-14 06:50:27 +0200

    removed aws configure

--- docker/copy_files/start_consul.sh
+++ docker/copy_files/start_consul.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-aws configure set region us-east-1
-aws configure set output text
-
 # Start Consul agent
 consul agent -dev -client "0.0.0.0" -config-file=/usr/lib/consul/encrypt_consul.json > /var/log/consul/consul.out &
 
