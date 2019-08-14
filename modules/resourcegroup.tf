dcaf94221766bbd2ac695eae6dd3f63c44ce0e77
Marko Kljajic <marko_kole@yahoo.com>
2019-08-14 06:50:05 +0200

    added count

--- modules/resourcegroup.tf
+++ modules/resourcegroup.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "resource_group" {
+    count                   = "${local.provision_resource_group}"
     name                    = "${local.resource_group_name}"
     location                = "${local.location}"
 }
