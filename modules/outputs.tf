761c8ba1b5cf632b17c2502a814f5533be15f989
Marko Kljajic <marko_kole@yahoo.com>
2019-08-14 06:53:33 +0200

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-    value           = "${azurerm_resource_group.resource_group.name}"
+    value           = "${azurerm_resource_group.resource_group.*.name[0]}"
 }
 output "databricks_workspace_id" {
     depends_on      = ["azurerm_databricks_workspace.databricks_workspace"]
